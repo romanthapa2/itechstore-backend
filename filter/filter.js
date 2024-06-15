c5d40f3c704a0bee160e08ef62e8c64ac4f34d15
roman thapa <thaparoman970@gmail.com>
2024-06-15 17:18:55 +0545

    update : moving for readability

--- filter/filter.js
+++ filter/filter.js
@@ -1,42 +1,62 @@
-// const express = require("express");
-// const router = express.Router();
-// const admin = require("../modules/admin");
+const express = require("express");
+const router = express.Router();
+const admin = require("../modules/admin");
 
-// router.get("/filtered-data", async (req, res) => {
-//   try {
-//     // Retrieve filtering criteria from query parameters
-//     const { type } = req.query;
+// route: 1 filter data of type http://localhost:5000/api/admin/filtered-data?type=Laptop
+router.get("/filtered-data", async (req, res) => {
+    try {
+  
+      // Query the database with the filter object
+      const filteredData = await admin.find({ type: req.query.type });
+  
+      // Return the filtered data as JSON response
+      res.json(filteredData);
+    } catch (error) {
+      // Handle errors
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+
+  // route: 2
+router.get("/filtered-brand", async (req, res) => {
+    try {
+      // Retrieve filtering criteria from query parameters
+  
+  
+      // Query the database with the filter object
+      const filteredData = await admin.find({ brand: req.query.brand });
+  
+      // Return the filtered data as JSON response
+      res.json(filteredData);
+      console.log("finding brand")
+      console.log(filteredData);
+    } catch (error) {
+      // Handle errors
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
 
 
-//     // Query the database with the filter object
-//     const filteredData = await admin.find({ type: type });
+  // route 3. get data by id
+router.get("/laptop/:id", async (req, res) => {
+    try {
+      const id = req.params.id
+      if (!id) {
+        return res.status(404).json({ message: "id parameter is missing" })
+      }
+  
+      const findLaptopById = await admin.findById(id);
+  
+      if (!findLaptopById) {
+        res.status(403).json({ message: "Laptop not found" })
+      }
+  
+      res.json(findLaptopById);
+    } catch (error) {
+      res.status(500).json({ message: `Internal server error ${error}` })
+    }
+  })
+  
+  
 
-//     // Return the filtered data as JSON response
-//     res.json(filteredData);
-//     console.log(filteredData);
-//   } catch (error) {
-//     // Handle errors
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// });
-
-// router.get("/filtered-brand", async (req, res) => {
-//   try {
-//     // Retrieve filtering criteria from query parameters
-//     const { brand } = req.query;
-//     console.log(brand)
-
-
-//     // Query the database with the filter object
-//     const filteredData = await admin.find({ brand: brand });
-
-//     // Return the filtered data as JSON response
-//     res.json(filteredData);
-//     console.log(filteredData);
-//   } catch (error) {
-//     // Handle errors
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// });
-
-// module.exports = router;
+module.exports = router;
