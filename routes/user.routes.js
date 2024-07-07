a8cb03fb6163b4c603e6bf1f2eab7f1f4568b41c
roman thapa <thaparoman970@gmail.com>
2024-07-07 09:15:10 +0545

    update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,17 +3,16 @@ const {registerUser, loginUser} = require("../controller/user.controller.js");
 const {body} = require("express-validator");
 
 const router = express.Router();
-[
-    body('username')
-      .isLength({ min: 5 }).withMessage('Username must be at least 5 characters long')
-      .isAlphanumeric().withMessage('Username must contain only letters and numbers'),
-    body('email')
-      .isEmail().withMessage('Email is not valid'),
-    body('password')
-      .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
-  ],
+
 router.route('/register').post(
-    
+    [
+        body('name')
+          .isLength({ min: 5 }).withMessage('Username must be at least 5 characters long'),
+        body('email')
+          .isEmail().withMessage('Email is not valid'),
+        body('password')
+          .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+      ],
 registerUser
 )
 
