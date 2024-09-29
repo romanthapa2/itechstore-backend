39af2de34a98b82f54ba97c1305e76221fded8df
romanthapa2 <thaparoman970@gmail.com>
2024-09-29 11:36:53 +0545

    error sending to fontend

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
 const express = require("express");
-const verifyAdminByJwt = require("./middleware/auth.middlware.js")
+const verifyAdminByJwt = require("./middleware/auth.middlware.js");
 
 const app = express();
 
@@ -9,16 +9,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 function cacheImages(req, res, next) {
-    if (req.url.endsWith(".jpg") || req.url.endsWith(".jpeg") || req.url.endsWith(".png") || req.url.endsWith(".webp")) {
-        res.setHeader("Cache-Control", "public, max-age=31536000");
-    }else {
-        res.setHeader("Cache-Control", "public, max-age=0");  
-    }
-    next();
+  if (
+    req.url.endsWith(".jpg") ||
+    req.url.endsWith(".jpeg") ||
+    req.url.endsWith(".png") ||
+    req.url.endsWith(".webp")
+  ) {
+    res.setHeader("Cache-Control", "public, max-age=31536000");
+  } else {
+    res.setHeader("Cache-Control", "public, max-age=0");
+  }
+  next();
 }
 
 app.use("/uploads", cacheImages, express.static("uploads"));
 
+app.use((err, req, res, next) => {
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+      errors: err.errors || [],
+    });
+  }
+
+  return res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+});
 
 const adminRoutes = require("./routes/admin.routes.js");
 const filterRoutes = require("./routes/filter.routes.js");
