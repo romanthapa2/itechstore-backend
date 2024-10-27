3ab59050c87959711b7f409de70110c9cd0e8088
romanthapa2 <thaparoman970@gmail.com>
2024-10-27 13:25:37 +0545

    update on product model

--- models/product.model.js
+++ models/product.model.js
@@ -1,12 +1,43 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const productSchema = new Schema({
-  name: { type: String, required: true },
-  type: { type: String, required: true },
-  brand: { type: String, required: true },
-  desc: { type: String},
-  price: { type: String, required: true },
-  img: {type: String, required: true},
+  name: { type: String, required: [true,"Please Enter the Name"] },
+  type: { type: String, required: [true,"Please Enter the Type"] },
+  brand: { type: String, required: [true,"Please Enter the Brand"] },
+  desc: { type: String , required: [true,"Please Enter the Description"]},
+  price: { type: String, required: [true,"Please Enter the Price"] },
+  img: { type: String, required: [true,"Please Select the Image"] },
+  stock: {
+    type: Number,
+    required: [true, "Please Enter The Product Stock"],
+    maxlength: [4, "Stock cannot exceed 4 characters"],
+    default: 1,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
   date: { type: Date, default: Date.now },
 });
 
