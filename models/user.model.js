31289a812096f6f9721ecaf25ad46f92964b6331
romanthapa2 <thaparoman970@gmail.com>
2024-10-28 21:23:22 +0545

    update resetpasswordToken

--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,21 @@
 const mongooes = require("mongoose");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
 const { Schema } = mongooes;
 
 const userSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role : {type : String, default : "guest", enum : ["user","admin","guest"]},
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validator: [validator.isEmail, "please enter valid email"],
+  },
+  password: { type: String, required: true ,minLength:[6,"name cannot be less than 6 character"], },
+  role: { type: String, default: "guest", enum: ["user", "admin", "guest"] },
   date: { type: Date, default: Date.now },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
 
 userSchema.pre("save", async function (next) {
@@ -17,17 +25,34 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.isPasswordCorrect = async function(password){
-  return await bcrypt.compare(password, this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken = async function(){
-  return jwt.sign({
-    _id : this._id,
-  },process.env.ACCSS_TOKEN_SECRET_KEY,{
-    expiresIn : process.env.ACCSS_TOKEN_SECRET_EXPREY
-  }
-)
-}
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.ACCSS_TOKEN_SECRET_KEY,
+    {
+      expiresIn: process.env.ACCSS_TOKEN_SECRET_EXPREY,
+    }
+  );
+};
+
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+  return resetToken;
+};
 
 module.exports = mongooes.model("User", userSchema);
