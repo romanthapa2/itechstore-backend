41140b8b36dde3c042fd8ada5381a139d134f0ed
romanthapa2 <thaparoman970@gmail.com>
2024-11-12 13:08:42 +0545

    validate new created product data

--- routes/filter.routes.js
+++ routes/filter.routes.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const {
   getDataByFilters,
-  getDataById,
   getDataBySearch,
 } = require("../controller/filter.controller.js");
 
 const router = express.Router();
 
 router.route("/dataByFilter").get(getDataByFilters);
-router.route("/dataById/:id").get(getDataById);
 router.route("/dataBySearch").get(getDataBySearch);
 
 module.exports = router;
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -9,14 +9,52 @@ const {
   getProductReviews,
   deleteReview,
 } = require("../controller/product.controller");
-const { verifyUserByJWT, verifyAdminByJwt } = require("../middleware/auth");
+const {
+  verifyUserByJWT,
+  verifyAdminByJwt,
+} = require("../middleware/auth.middlware");
 const router = express.Router();
 
+const validateLengthOfProductData = [
+  body("name")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("Username must be at least 5 characters long")
+    .escape(),
+  body("type")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("type must be at least 5 characters long")
+    .escape(),
+  body("desc")
+    .trim()
+    .isLength({ min: 10 })
+    .withMessage("Description must be at least 10 characters long")
+    .escape(),
+  body("price")
+    .trim()
+    .isNumeric()
+    .withMessage("Price must be a numeric value")
+    .escape(),
+];
 
 router.route("/products").get(getAllProducts);
-router
-  .route("/product/new")
-  .post(verifyAdminByJwt, createProduct);
+router.route("/product/new").post(
+  verifyAdminByJwt,
+  validateLengthOfProductData,
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new ApiError(
+        400,
+        errors.array().map((err) => err.msg)
+      );
+    }
+    next();
+  },
+  upload.single("img"),
+  createProduct
+);
 router
   .route("/product/:id")
   .put(verifyAdminByJwt, updateProduct)
